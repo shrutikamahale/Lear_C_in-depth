30fdbe087c1b5362fde6c0b5272bbd460b97f16b
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-18 20:01:38 +0530

    Add files via upload
    while loop exercises

---
+++ while_1.c
@@ -0,0 +1,14 @@
+//Program to print 1 to 10 number using while loop.
+#include<stdio.h>
+int main()
+{
+    int number = 1;
+    while (number <= 10);
+    {
+        printf("No: are %d\n", number);
+        number = number+1;
+        
+    }
+    printf("\n");
+    
+}
