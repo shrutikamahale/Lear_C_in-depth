50aeb41715d4e6ae3feff135d90e1b2c200a086b
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-18 20:02:10 +0530

    Add files via upload

---
+++ while_2.c
@@ -0,0 +1,17 @@
+//Program to print numbers in revenrse orders with a difference of 2*
+
+#include<stdio.h>
+int main()
+{
+    int number = 10;
+    
+    while(number >= 2)
+    {
+        printf("No. are as follows %d\n", number);
+        number = number - 2;
+    }
+
+    printf("\n");
+    
+    
+}
