9912ecd58e9ba4294c2a0850da625ed72b776405
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-19 18:27:09 +0530

    Add files via upload

---
+++ do_while_1.c
@@ -0,0 +1,13 @@
+//Program to print 1 to 10 using do while loop.
+#include<stdio.h>
+int main()
+{
+    int i = 1;
+    do
+    {
+        printf("%d\n", i);
+        i = i+1;
+    }while (i <= 10);
+    printf("\n");
+    
+}
