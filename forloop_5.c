5ec0f498defd50546ad9c49d33762d39aa68296b
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-20 22:17:01 +0530

    Add files via upload
    //Program to generate fibonacci series

---
+++ forloop_5.c
@@ -0,0 +1,22 @@
+//Program to generate fibonacci series
+#include<stdio.h>
+int main()
+{
+    long x,y,z;
+    int i, n;
+    x = 0;
+    y = 1;
+    printf("Enter the number of terms: \n");
+    scanf("%d", &n);
+    printf("ld ", y);
+
+    for(i = 1; i < n; i++)
+    {
+        z = x + y;
+        printf("%ld ", z);
+        x = y;
+        y = z;
+    }
+    printf("\n");
+
+}
