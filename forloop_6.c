0101ee205f89814bf90dd67e0962f1ef4375646d
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-21 09:07:26 +0530

    Add files via upload
    //program to print sum of digits of any number using for loop

---
+++ forloop_6.c
@@ -0,0 +1,15 @@
+//program to print sum of digits of any number using for loop
+#include<stdio.h>
+int main()
+{
+    int i, rem, sum = 0;
+    printf("Enter the number: ");
+    scanf("%d", &i);
+    for(;i > 0; i/=10)
+    {
+        rem = i % 10;
+        sum = sum + rem;
+    }
+    printf("Sum of digits: %d\n", sum);
+
+}
