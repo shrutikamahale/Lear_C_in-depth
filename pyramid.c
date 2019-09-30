6d1ed3ddb0a6ffb03d47f4259f5aabd8444877e9
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-30 17:16:54 +0530

    Add files via upload
    //write a program to print a pyramid like
         *
         * *
         * * *
         * * * *
         * * * * *

---
+++ pyramid.c
@@ -0,0 +1,20 @@
+//write a program to print a pyramid like
+//     *
+//     * *
+//     * * *
+//     * * * *
+//     * * * * *
+#include <stdio.h>
+int main()
+{
+  int i,j,n;
+  printf("Enter n: ");
+  scanf("%d", &n);
+
+  for(i = 1; i <= n; i++)
+  {
+    for(j = 1; j<= i; j++)
+          printf("* ");
+    printf("\n");
+  }
+}
