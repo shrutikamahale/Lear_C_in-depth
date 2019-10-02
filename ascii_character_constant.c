50afe77459d86fe5784739a85444a88a792d4a3d
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-10-02 21:57:46 +0530

    Add files via upload
    //Write a program to display ASCII values for character constants ‘\n’, ‘\r’, ‘\t’, ‘\a’, ‘\b’.

---
+++ ascii_character_constant.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  printf("ASCII value of \\n = %d\n",'\n');
+  printf("ASCII value of \\r = %d\n",'\r');
+  printf("ASCII value of \\t = %d\n",'\t');
+  printf("ASCII value of \\a = %d\n",'\a');
+  printf("ASCII value of \\b = %d\n",'\b');
+}
