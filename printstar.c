a8d7e158c70849e9aee4cb979018a50e638dfde4
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-10-02 21:59:40 +0530

    Add files via upload
    //Write a program to accept three integer numbers and find its average.

---
+++ printstar.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+int main(void)
+{
+  printf("*\n");
+  printf("* *\n");
+  printf("* * *\n");
+  printf("* * * *\n");
+  printf("* * * * *\n");
+}
+
+//using single print statement
+#include<stdio.h>
+int main(void)
+{
+   printf("*\n* *\n* * *\n* * * *\n* * * * *");
+}
